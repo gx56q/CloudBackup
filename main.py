52e68f1f73d2c53490954610e9bfd2365d4a2df3
n1ghtm <andrei.voinov@urfu.me>
2023-05-22 21:28:21 +0500

    delete old argparse

--- main.py
+++ main.py
@@ -1,56 +1,59 @@
-import os
-from dotenv import load_dotenv, find_dotenv
-import asyncio
-import argparse
-
+import click
 from ftp_client.ftpclient import FtpClient
 from yadisk_client.yadiskclient_webdav import YaDiskClient
 
 
-async def main():
-    parser = argparse.ArgumentParser(description="FTP client")
-    parser.add_argument("host", help="FTP server hostname")
-    parser.add_argument("-t", "--type", choices=["ftp", "yadisk"], default="ftp", help="Client type")
-    parser.add_argument("-p", "--port", type=int, default=21, help="FTP server port")
-    parser.add_argument("-u", "--username", required=True, help="FTP server username")
-    parser.add_argument("-pw", "--password", required=True, help="FTP server password")
-    parser.add_argument("-d", "--download", nargs=2, metavar=("REMOTE_PATH", "LOCAL_PATH"), help="Download file or directory from the FTP server")
-    parser.add_argument("-up", "--upload", nargs=2, metavar=("LOCAL_PATH", "REMOTE_PATH"), help="Upload file or directory to the FTP server")
-    parser.add_argument("-l", "--list", metavar="REMOTE_PATH", help="List files in the given directory on the FTP server")
-    args = parser.parse_args()
-    if args.type == "ftp":
+@click.command()
+@click.option('-c', '--client', type=click.Choice(['ftp', 'yadisk']), required=True, help='Type of client to use.')
+@click.option('--host', type=str, metavar='HOST', help='FTP host.')
+@click.option('-u', '--user', type=str, metavar='USERNAME', help='Username.')
+@click.option('-p', '--pass', 'password', type=str, metavar='PASSWORD', help='Password.')
+@click.option('-t', '--token', type=str, metavar='TOKEN', help='OAuth token.')
+@click.option('-d', '--download', nargs=2, type=str, metavar=('REMOTE_PATH', 'LOCAL_PATH'),
+              help='Download a file from the remote path to the local path.')
+@click.option('-up', '--upload', nargs=2, type=str, metavar=('LOCAL_PATH', 'REMOTE_PATH'),
+              help='Upload a file from the local path to the remote path.')
+@click.option('-l', '--list', 'list_files', type=str, metavar='REMOTE_PATH', help='List files at the remote path.')
+def cli(client, host, user, password, token, download, upload, list_files):
+    if client == 'ftp':
+        if not host or not user or not password:
+            raise click.UsageError('For FTP client, --host, --user, and --pass are required.')
         ftp = FtpClient()
-        ftp.connect(args.host, args.username, args.password)
-        print(f"Connected to {args.host}")
-        if args.download:
-            remote_path, local_path = args.download
+        ftp.connect(host, user, password)
+        if download:
+            remote_path, local_path = download
             ftp.download(remote_path, local_path)
-            print(f"Downloaded {remote_path} to {local_path}")
-        elif args.upload:
-            local_path, remote_path = args.upload
+        if upload:
+            local_path, remote_path = upload
             ftp.upload(local_path, remote_path)
-            print(f"Uploaded {local_path} to {remote_path}")
-        elif args.list:
-            remote_path = args.list
-            files = ftp.list(remote_path, True)
+        if list_files:
+            remote_path = list_files
+            ftp.list(remote_path, True)
         ftp.close()
-    elif args.type == "yadisk":
-        pass
+
+    elif client == 'yadisk':
+        yadisk = YaDiskClient()
+        if token:
+            yadisk.set_token(token)
+        elif user and password:
+            yadisk.set_auth(user, password)
+        else:
+            raise click.UsageError('For Yandex Disk client, --user and --pass or --token are '
+                                   'required.')
+        if download:
+            remote_path, local_path = download
+            yadisk.download(remote_path, local_path)
+            click.echo(f"Yandex Disk: Downloaded {remote_path} to {local_path}")
+        if upload:
+            local_path, remote_path = upload
+            yadisk.upload(local_path, remote_path)
+            click.echo(f"Yandex Disk: Uploaded {local_path} to {remote_path}")
+        if list_files:
+            directory = list_files
+            yadisk.list_directory_recursive(directory)
+            click.echo(f"Yandex Disk: Listing files at {directory}")
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
-    # ftp = FtpClient()
-    # ftp.connect(host, username, password)
-    # ftp.download('9833-E243/test/', '/local/dir')
-    # ftp.upload('/local/dir/remote_file.txt', '9833-E243/test/remote_file.txt')
-    # ftp.list('9833-E243/test', True)
-    # ftp.close()
-    # Загрузка переменных окружения из файла config.env
-    load_dotenv(find_dotenv('config.env'))
-    YA_TOKEN = os.getenv('YADISK_TOKEN')
-    YA_LOGIN = os.getenv('YADISK_USER')
-    YA_PASSWORD = os.getenv('YADISK_PASS')
-    FTP_HOST = os.getenv('FTP_HOST')
-    FTP_USER = os.getenv('FTP_USER')
-    FTP_PASS = os.getenv('FTP_PASS')
+    cli()
 
