ecfaa822b4843284f15e8e2420c2cc1d721913d8
n1ghtm <andrei.voinov@urfu.me>
2023-05-23 19:34:40 +0500

    add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CloudBackup',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/gx56q/CloudBackup',
+    license='MIT License',
+    author='gx56q',
+    author_email='',
+    description=''
+)
